bb3369ff26efad5cafa2e82070e0d695c1321b70
fahri42kymz <fahri42kymz@gmail.com>
2026-08-24 12:18:27 +0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'url'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  optimizeDeps: {
+    exclude: ['@react-three/rapier'],
+  },
+  build: {
+    target: 'esnext',
+    chunkSizeWarningLimit: 1600,
+  },
+})
+
